3090fe3d39f7b7ac9156633c5354258aacf28d38
Moelmi89 <moelmi89@gmail.com>
2023-03-08 12:21:05 +0000

    tag routes completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category}, {model: Tag, through: Product, as: 'product_tag'}],
+      include: [{ model: Category}, {model: Tag, through: Product, as: 'product_tags'}],
     });
       res.status(200).json(productData);
     } catch (err) {
@@ -19,8 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      // JOIN with locations, using the Trip through table
-      include: [{ model: Category}, {model: Tag, through: Product, as: 'product_tag'}],
+      include: [{ model: Category}, {model: Tag, through: Product, as: 'product_tags'}],
     });
 
     if (!productData) {
