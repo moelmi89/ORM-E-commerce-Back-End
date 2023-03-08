a1612dfca11b4a78a87fbf878c9085b5f8fd8373
Moelmi89 <moelmi89@gmail.com>
2023-03-08 12:02:47 +0000

    product routes complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,10 +15,23 @@ router.get('/', async (req, res) => {
     }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// get one product  // find a single product by its `id`/ be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Category}, {model: Tag, through: Product, as: 'product_tag'}],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -95,8 +108,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-});
+  router.delete('/:id', async (req, res) => {
+    try {
+      const productData = await Product.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(productData);
+      console.log("Category Successfully Deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
