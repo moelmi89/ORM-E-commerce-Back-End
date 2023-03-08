77ec8a0ba1212137d07b495e05ad455cf2debef9
Moelmi89 <moelmi89@gmail.com>
2023-03-08 11:49:13 +0000

    product routes started

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,10 +3,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// get all products  // find all products// be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category}, {model: Tag, through: Product, as: 'product_tag'}],
+    });
+      res.status(200).json(productData);
+    } catch (err) {
+    res.status(500).json(err);
+    }
 });
 
 // get one product
